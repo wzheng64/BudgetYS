ab1af84682d0a3669f1450f3fcdd33221702ed37
BuildTools <unconfigured@null.spigotmc.org>
2020-08-21 21:22:09 -0400

    Form created for making account

--- src/app/main/account-edit/account-edit.component.ts
+++ src/app/main/account-edit/account-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AccountType } from '../../shared/enums';
 
 @Component({
   selector: 'app-account-edit',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-edit.component.css']
 })
 export class AccountEditComponent implements OnInit {
+  accountForm: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.accountForm = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      type: new FormControl('CC', Validators.required),
+      transactions: new FormControl([]),
+      balance: new FormControl(0, Validators.pattern(/^[0-9]*\.[0-9]{2}$/))
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.accountForm);
+  }
+
+  public getTypes(): string[] {
+    const types = [];
+    for (const t in AccountType) {
+      if (isNaN(Number(t))) {
+        types.push(t);
+      }
+    }
+    return types;
   }
 
 }
