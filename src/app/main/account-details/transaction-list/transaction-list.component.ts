b4cdb18f878466bda6b0b75f9255c673c89fd6f3
BuildTools <unconfigured@null.spigotmc.org>
2020-08-26 12:32:34 -0400

    Transaction list now properly updates when switching accounts

--- src/app/main/account-details/transaction-list/transaction-list.component.ts
+++ src/app/main/account-details/transaction-list/transaction-list.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AccountService } from 'src/app/shared/account.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
@@ -11,16 +12,21 @@ import { Transaction } from 'src/app/shared/transaction.model';
 export class TransactionListComponent implements OnInit, OnDestroy {
   transactions: Transaction[];
   transSub: Subscription;
+  pSub: Subscription;
 
-  constructor(private accountService: AccountService) { }
+  constructor(private accountService: AccountService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.transSub = this.accountService.transactionsChanged.subscribe((trans: Transaction[]) => {
       this.transactions = trans;
     });
+    this.pSub = this.route.params.subscribe((params: Params) => {
+      this.transactions = this.accountService.getTransactions(params.id);
+    });
   }
 
   ngOnDestroy(): void {
     this.transSub.unsubscribe();
+    this.pSub.unsubscribe();
   }
 }
