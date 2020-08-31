86300965767f7e01e90ed6f1282217d314b54424
BuildTools <unconfigured@null.spigotmc.org>
2020-08-31 14:57:16 -0400

    Added total amount section to summary

--- src/app/main/summary/summary.component.ts
+++ src/app/main/summary/summary.component.ts
@@ -1,3 +1,6 @@
+import { Account } from './../../shared/account.model';
+import { Subscription } from 'rxjs';
+import { AccountService } from './../../shared/account.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./summary.component.css']
 })
 export class SummaryComponent implements OnInit {
+  total: number;
+  accSub: Subscription;
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.accountService.accountsChanged.subscribe((accounts: Account[]) => {
+      this.setTotal(accounts);
+    });
+    this.setTotal(this.accountService.getAccounts());
+  }
+
+  private setTotal(accs: Account[]): void {
+    this.total = 0;
+    accs.forEach((acc) => {
+      if (acc.type !== 'CC') {
+        this.total += acc.balance;
+      }
+      else {
+        this.total -= acc.balance;
+      }
+    });
+    this.total = Number(this.total.toFixed(2));
   }
 
 }
